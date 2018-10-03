d7566424b1dfc6d4f056c9cf8606b4e9f8de6852
pshowland <pshowland13@my.trine.edu>
2018-10-03 15:49:21 +0000

    Added a command to list the current roles on a server.

--- bot.py
+++ bot.py
@@ -15,4 +15,16 @@ async def on_message(message):
         return_message = "Hello! I'm AU Bot, and I hope to be actually useful!"
         await client.send_message(message.channel, return_message)
 
+    if(message.content == "!roles"):
+        server_roles = []
+        for role in message.server.roles:
+            if(role.name != "@everyone"):
+                server_roles.append(role.name)
+        return_message = ""
+        if(len(server_roles) == 0):
+            return_message = "There are currently no roles set on this server!"
+        else:
+            return_message = "The current roles for this server are: " + str(server_roles)
+        await client.send_message(message.channel, return_message)
+
 client.run(TOKEN)
