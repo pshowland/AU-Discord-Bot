7c2dbc07f3364f2318234de5aa086ae6020e391f
pshowland <pshowland13@my.trine.edu>
2018-10-03 14:39:35 +0000

    Added the first command: AUBot introduction.

--- bot.py
+++ bot.py
@@ -11,4 +11,8 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if(message.content == "!aubot"):
+        return_message = "Hello! I'm AU Bot, and I hope to be actually useful!"
+        await client.send_message(message.channel, return_message)
+
 client.run(TOKEN)
